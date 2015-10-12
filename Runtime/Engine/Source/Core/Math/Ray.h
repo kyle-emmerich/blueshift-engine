c7fd04f654c1625047d3292f3c6cb0c23f130428
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-10-12 11:19:26 -0500

    Added Ray class

--- Runtime/Engine/Source/Core/Math/Ray.h
+++ Runtime/Engine/Source/Core/Math/Ray.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Core/Math/Vector.h"
 
 namespace Blueshift {
 	namespace Core {
@@ -8,8 +9,19 @@ namespace Blueshift {
 			struct Ray_ {
 				Vector<n, T> Origin;
 				Vector<n, T> Direction;
+				Vector<n, T> InvDirection;
 
 				T MaxDistance;
+
+				Ray_(Vector<n, T> origin, Vector<n, T> direction, T max_distance)
+					: Origin(origin), Direction(direction), MaxDistance(max_distance) {
+					InvDirection = T(1) / Direction;
+				}
+
+				void SetDirection(Vector<n, T> direction) {
+					Direction = direction;
+					InvDirection = T(1) / Direction;
+				}
 			};
 
 			typedef Ray_<3, double> Ray;
