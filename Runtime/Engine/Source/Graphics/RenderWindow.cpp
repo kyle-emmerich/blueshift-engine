b5efbfff7590d762f36df541f64e0bebae99b46b
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-10-25 20:42:20 -0500

    RenderWindow actually sets desired state now.

--- Runtime/Engine/Source/Graphics/RenderWindow.cpp
+++ Runtime/Engine/Source/Graphics/RenderWindow.cpp
@@ -6,9 +6,12 @@ using namespace Blueshift;
 using namespace Blueshift::Core;
 using namespace Blueshift::Graphics;
 
+
+
 RenderWindow::RenderWindow(uint32_t Width, uint32_t Height)
 	: Window(Width, Height, true), view_width(Width), view_height(Height)
 { 
+	view_id = static_cast<uint8_t>(Engine::Get().GetRenderSystem().GetRenderWindows().size());
 	if (Engine::Get().GetRenderSystem().GetRenderWindows().size() == 0) {
 		is_primary = true;
 		this->setup_platform();
@@ -29,15 +32,26 @@ void RenderWindow::PreRender() {
 	if (dirty_viewport) {
 		dirty_viewport = false;
 		if (is_primary) {
-			bgfx::reset(view_width, view_height, BGFX_RESET_VSYNC);
+			bgfx::setViewFrameBuffer(view_id, BGFX_INVALID_HANDLE);
+			bgfx::reset(view_width, view_height,
+				BGFX_RESET_VSYNC |
+				BGFX_RESET_MSAA_X4 |
+				BGFX_RESET_SRGB_BACKBUFFER
+				);
+
 		} else {
 			//update framebuffer
 		}
 	}
-	bgfx::setViewRect(0, 0, 0, view_width, view_height);
-	bgfx::touch(0);
-	bgfx::dbgTextClear();
-	bgfx::dbgTextPrintf(0, 0, 0x4f, "test");
+	bgfx::setViewRect(view_id, 0, 0, view_width, view_height);
+	bgfx::touch(view_id);
+	bgfx::setState(
+		BGFX_STATE_MSAA |
+		BGFX_STATE_ALPHA_WRITE |
+		BGFX_STATE_RGB_WRITE |
+		BGFX_STATE_CULL_CW |
+		BGFX_STATE_DEPTH_WRITE |
+		BGFX_STATE_DEPTH_TEST_LESS);
 }
 
 void RenderWindow::PostRender() {
