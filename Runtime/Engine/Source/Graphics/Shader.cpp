ebb8ee31637612649839a9be121e66f8a49b52b2
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-10-28 12:03:41 -0500

    Shader uniform handling should now be correct.

--- Runtime/Engine/Source/Graphics/Shader.cpp
+++ Runtime/Engine/Source/Graphics/Shader.cpp
@@ -4,12 +4,15 @@
 using namespace Blueshift;
 using namespace Graphics;
 
-Shader::Shader(uint8_t* data, size_t size) {
+Shader::Shader(uint8_t* data, size_t size)
+	: data(data) {
 	handle = bgfx::createShader(bgfx::makeRef(data, static_cast<uint32_t>(size)));
-	
+
 	uint16_t num_uniforms = bgfx::getShaderUniforms(handle);
-	uniform_handles.resize(num_uniforms);
-	bgfx::getShaderUniforms(handle, &(uniform_handles[0]));
+	if (num_uniforms > 0) {
+		uniform_handles.resize(static_cast<size_t>(num_uniforms));
+		bgfx::getShaderUniforms(handle, &(uniform_handles[0]), num_uniforms);
+	}
 }
 
 Shader::~Shader() {
@@ -30,8 +33,8 @@ Shader::~Shader() {
 }
 
 Shader& Shader::AddUniform(std::string Name, Shader::UniformType Type, size_t Size) {
-	if (uniforms.size() == uniform_handles.size()) {
-		throw 0; //TODO: add exception for too many uniforms
+	if (uniform_handles.size() <= uniforms.size()) {
+		throw 0; //TODO exception
 	}
 	Uniform* uniform = new Uniform;
 	uniform->Handle = uniform_handles[uniforms.size()];
@@ -39,6 +42,8 @@ Shader& Shader::AddUniform(std::string Name, Shader::UniformType Type, size_t Si
 	uniform->Name = Name;
 	uniform->Type = Type;
 	uniform->Size = Size;
+	uniform_handles.push_back(uniform->Handle);
+	uniforms.push_back(uniform);
 	named_uniforms[uniform->Name] = uniform;
 	return *this;
 }
