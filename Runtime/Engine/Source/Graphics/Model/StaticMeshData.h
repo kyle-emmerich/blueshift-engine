54e4b4ef7ccb2fd81844890d7063613233e7ac58
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-10-28 12:04:27 -0500

    StaticMeshData now accepts non-engine defined vertex types for loading the type from the GPU. Makes sense given that you can't modify static mesh data anyway, so you don't really need a nicely defined type.

--- Runtime/Engine/Source/Graphics/Model/StaticMeshData.h
+++ Runtime/Engine/Source/Graphics/Model/StaticMeshData.h
@@ -6,33 +6,41 @@ namespace Blueshift {
 	namespace Graphics {
 		namespace Model {
 
-			template<typename VertexType = StaticVertex,
-					 typename IndexType = uint16_t>
+			template<typename VertexT = StaticVertex,
+					 typename IndexT = uint16_t>
 			class StaticMeshData : public IMeshData {
 			protected:
 				bgfx::VertexBufferHandle vertex_buf;
 				bgfx::IndexBufferHandle index_buf;
 
-				VertexType* vertex_data;
+				VertexT* vertex_data;
+				IndexT* index_data;
 				size_t num_vertices;
-				IndexType* index_data;
 				size_t num_indices;
+
+				std::string name;
 
 				std::vector<MeshDataSection> sections;
 			public:
 				StaticMeshData() = delete;
 				StaticMeshData(
-					VertexType* Vertices, size_t NumVertices,
-					IndexType* Indices, size_t NumIndices)
-					: vertex_data(Vertices), num_vertices(NumVertices),
-					  index_data(Indices), num_indices(NumIndices) {
+					std::string name,
+					VertexT* Vertices, size_t NumVertices, VertexType& Type,
+					IndexT* Indices, size_t NumIndices, MeshBounds Bounds)
+					: num_vertices(NumVertices), num_indices(NumIndices) {
+
+					vertex_data = new VertexT[NumVertices * Type.getStride()];
+					index_data = new IndexT[NumIndices * sizeof(IndexT)];
+
+					memcpy(vertex_data, Vertices, NumVertices * Type.getStride());
+					memcpy(index_data, Indices, NumIndices * sizeof(IndexT));
 
 					//Are we using 32-bit indices?
 					uint16_t index_flags = BGFX_BUFFER_NONE;
-					if (typeid(IndexType) == typeid(uint32_t)) {
+					if (sizeof(IndexT) == sizeof(uint32_t)) {
 						//We are, set the flag
 						index_flags = BGFX_BUFFER_INDEX32;
-					} else if (typeid(IndexType) == typeid(uint16_t)) {
+					} else if (sizeof(IndexT) == sizeof(uint16_t)) {
 						//Do nothing, no flags to set for 16-bit indices.
 					} else {
 						//TODO: Throw an error for an unsupported index type?
@@ -41,12 +49,12 @@ namespace Blueshift {
 					}
 					
 					vertex_buf = bgfx::createVertexBuffer(
-						bgfx::makeRef(vertex_data, num_vertices * sizeof(VertexType)),
-						VertexType::decl,
+						bgfx::makeRef(vertex_data, static_cast<uint32_t>(num_vertices * sizeof(VertexT))),
+						Type,
 						BGFX_BUFFER_NONE
 					);
 					index_buf = bgfx::createIndexBuffer(
-						bgfx::makeRef(index_data, num_indices * sizeof(IndexType)),
+						bgfx::makeRef(index_data, static_cast<uint32_t>(num_indices * sizeof(IndexT))),
 						index_flags
 					);
 
@@ -54,44 +62,44 @@ namespace Blueshift {
 					//modified later.
 					sections.push_back({
 						0, num_vertices,
-						0, num_indices
+						0, num_indices,
+						Bounds
 					});
 				}
 				//It doesn't make much sense to have copy/move on a static data class.
 				//You can only use it to render after instantiation, so just pass it
 				//as a reference or pointer.
-				StaticMeshData(StaticMeshData& data) = delete;
-				StaticMeshData(StaticMeshData&& data) = delete;
+				StaticMeshData(const StaticMeshData& data) = delete;
+				StaticMeshData(const StaticMeshData&& data) = delete;
 
 				virtual ~StaticMeshData() {
-					if (vertex_buf != BGFX_INVALID_HANDLE) {
+					if (vertex_buf.idx != bgfx::invalidHandle) {
 						bgfx::destroyVertexBuffer(vertex_buf);
 						vertex_buf = BGFX_INVALID_HANDLE;
 					}
-					if (index_buf != BGFX_INVALID_HANDLE) {
+					if (index_buf.idx != bgfx::invalidHandle) {
 						bgfx::destroyIndexBuffer(index_buf);
 						index_buf = BGFX_INVALID_HANDLE;
 					}
 
 					if (vertex_data != nullptr) {
 						delete[] vertex_data;
-						vertex_data = nullptr;
 					}
-					
 					if (index_data != nullptr) {
 						delete[] index_data;
-						index_data = nullptr;
 					}
 				}
 
-				size_t AddSection(size_t VertexStart, size_t VertexNumber, size_t IndexStart, size_t IndexNumber) {
+				size_t AddSection(size_t VertexStart, size_t VertexNumber, size_t IndexStart, size_t IndexNumber, MeshBounds Bounds) {
 					size_t index = sections.size();
 					sections.push_back({
 						VertexStart, VertexNumber,
-						IndexStart, IndexNumber
+						IndexStart, IndexNumber,
+						Bounds
 					});
 					return index;
 				}
+
 
 				inline virtual void Render(ShaderProgram* program, size_t SectionID = 0) {
 					if (SectionID >= sections.size()) {
@@ -105,9 +113,16 @@ namespace Blueshift {
 					//TODO: make sure section isn't null...
 					//TODO: make sure program is valid
 					//TODO: replace programhandle with api-friendly wrap
-					bgfx::setVertexBuffer(vertex_buf, Section->VertexStart, Section->VertexNumber);
-					bgfx::setIndexBuffer(index_buf, Section->IndexStart, Section->IndexNumber);
-					bgfx::submit(Core::Engine::GetRenderSystem().GetCurrentViewID(), program->GetHandle());
+					bgfx::setVertexBuffer(vertex_buf, static_cast<uint32_t>(Section->VertexStart), static_cast<uint32_t>(Section->VertexNumber));
+					bgfx::setIndexBuffer(index_buf, static_cast<uint32_t>(Section->IndexStart), static_cast<uint32_t>(Section->IndexNumber));
+					bgfx::submit(Core::Engine::Get().GetRenderSystem().GetCurrentViewID(), program->GetHandle());
+				}
+
+				inline virtual const std::string& GetName() const {
+					return name;
+				}
+				inline virtual const MeshDataSection& GetSection(size_t SectionID) const {
+					return sections[SectionID];
 				}
 			};
 
