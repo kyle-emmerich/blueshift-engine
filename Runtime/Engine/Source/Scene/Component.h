027ba34a8342f27c9f654fab67edf2aaffcc3cd6
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-11-18 13:17:12 -0600

    Added Component::Handle struct; standby for detailed documentation. For now, you can do SomeComponent* x = handle; since it carries a T* operator. Might be a bit strange though.

--- Runtime/Engine/Source/Scene/Component.h
+++ Runtime/Engine/Source/Scene/Component.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <typeindex>
 
 namespace Blueshift {
 	namespace Scene {
@@ -8,9 +9,39 @@ namespace Blueshift {
 		class Component {
 		protected:
 			Object* object;
-			Component(SceneGraph*, Object* obj = nullptr) : object(obj) {}
+			std::type_index end_type;
+			Component(SceneGraph*, Object* obj = nullptr) : object(obj), end_type(typeid(Component)) {}
+
+#pragma pack(push, 8)
+			struct serialized {};
+#pragma pack(pop)
 		public:
 			virtual ~Component() {}
+
+			inline const std::type_index& GetEndType() const {
+				return end_type;
+			}
+
+			struct Handle {
+				size_t idx;
+				std::type_index type;
+				SceneGraph* graph;
+
+				Component* operator->() const;
+				template<typename T>
+				T* Get() const {
+					return dynamic_cast<T*>(operator->());
+				}
+				template<typename T>
+				operator T*() const {
+					return dynamic_cast<T*>(operator->());
+				}
+
+				Handle(SceneGraph* graph, size_t idx, const std::type_index& type)
+					: graph(graph), idx(idx), type(type) {}
+				Handle()
+					: graph(nullptr), idx(999999999), type(typeid(Component)) {}
+			};
 		};
 
 	}
--- Runtime/Engine/Source/Scene/Object.h
+++ Runtime/Engine/Source/Scene/Object.h
@@ -5,22 +5,37 @@
 #include <string>
 
 #include "Scene/Component.h"
+#include "Core/Serialize/Serialize.h"
 
 namespace Blueshift {
 	namespace Scene {
 
 		class Object {
 		private:
-			std::vector<Component*> components;
-			Core::Math::Matrix4f transform;
+			std::vector<Component::Handle> components;
+			Core::Math::Matrix4f transform = Core::Math::IdentityMatrix4f;
 
 			std::string name;
 		protected:
+#pragma pack(push, 8)
+			struct serialized {
+				Core::Serialize::SerializedString name;
+				Core::Serialize::SerializedList components;
 
+				Core::Math::Matrix4f transform;
+			};
+#pragma pack(pop)
 		public:
 
 			inline const Core::Math::Matrix4f& GetTransform() const {
 				return transform;
+			}
+			inline void SetTransform(const Core::Math::Matrix4f& trans) {
+				transform = trans;
+			}
+
+			inline const std::vector<Component::Handle>& GetComponents() const {
+				return components;
 			}
 		};
 
