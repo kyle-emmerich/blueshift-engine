0f91b3f359886bdddc41ebec89715681fac19647
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-11-30 02:44:45 -0600

    DebugConsole now has access to Lua

--- Runtime/Engine/Source/Core/DebugConsole.h
+++ Runtime/Engine/Source/Core/DebugConsole.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Scripting/LuaState.h"
 #include <string>
 #include <deque>
 
@@ -17,9 +18,23 @@ namespace Blueshift {
 			size_t show;
 			size_t max_size;
 			bool visible = false;
+
+			char input[256];
+			size_t input_cursor = 0;
+			Scripting::LuaState* lua = nullptr;
+
+			void write_line(const std::string& line, uint8_t attr);
+			void text_input(char c);
 		public:
 			DebugConsole(size_t num_shown = 10, size_t max_size = 100);
 			~DebugConsole();
+
+			inline void SetLuaState(Scripting::LuaState* state) {
+				lua = state;
+			}
+			inline Scripting::LuaState* GetLuaState() {
+				return lua;
+			}
 
 			void Render();
 
