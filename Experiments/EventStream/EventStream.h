b0b88721c418cd4c8f8875583ff55defe4d3c417
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-12-12 20:13:42 -0600

    EventStream experiment is now threaded. Right now, you can only write and read exclusively. Writing while reading coming soon.

--- Experiments/EventStream/EventStream.h
+++ Experiments/EventStream/EventStream.h
@@ -6,79 +6,142 @@
 #include <memory>
 #include <iostream>
 #include <mutex>
+#include <thread>
+#include <condition_variable>
+#include <atomic>
 
 template<typename T>
 class EventStream {
 protected:
 	T* buffer;
 	T* buffer_end;
-	T* head;
-	T* tail;
-	T* frame_head;
-	T* frame_tail;
+	std::atomic<T*> head;
+	std::atomic<T*> tail;
+	std::atomic<T*> frame_head;
+	std::atomic<T*> frame_tail;
 	size_t count;
-	size_t lost;
+	std::atomic<size_t> dropped;
 	size_t capacity;
 
-//	std::mutex read_mutex;
-//	std::mutex write_mutex;
+	std::mutex mutex;
+	bool writing;
+	bool reading;
+	std::condition_variable write_done;
+	std::condition_variable read_done;
+	std::unique_lock<std::mutex> write_lock;
+	std::unique_lock<std::mutex> read_lock;
 public:
-	inline EventStream(size_t BufferSize) {
+	inline EventStream(size_t BufferSize)
+		: mutex(), write_lock(mutex, std::defer_lock), read_lock(mutex, std::defer_lock) {
 		buffer = new T[BufferSize];
 		buffer_end = buffer + BufferSize;
 		head = buffer;
 		tail = buffer;
 		count = 0;
-		lost = 0;
 		capacity = BufferSize;
 	}
 	inline ~EventStream() {
 		delete[] buffer;
 	}
 
+	inline void Initialize() {
+		writing = true;
+		reading = false;
+		read_done.notify_one();
+	}
+
 	inline void BeginWrite() {
-		frame_tail = head;
-		tail = frame_tail;
+		//Begin critical section
+		write_lock.lock();
+		while (reading) {
+			read_done.wait(write_lock);
+		}
+		writing = true;
+		frame_tail = head.load();
+		tail = frame_tail.load();
 		count = 0;
+		dropped = 0;
+	}
+	inline bool TryWrite() {
+		if (write_lock.try_lock()) {
+			while (reading) {
+				read_done.wait(write_lock);
+			}
+			writing = true;
+			frame_tail = head.load();
+			tail = frame_tail.load();
+			count = 0;
+			return true;
+		}
+		return false;
 	}
 	inline void EndWrite() {
-		frame_head = head;
-		//something else maybe?
+		writing = false;
+		reading = true;
+		frame_tail = frame_head.load();
+		frame_head = head.load();
+		write_lock.unlock();
+		write_done.notify_one();
+		//End critical section
 	}
 
 	inline T* PushEvent() {
-		//If we go past the end of the buffer, reset to the beginning
-		if (head == buffer_end) {
+		count++;
+		if (count > capacity) {
+			dropped++;
+			count = capacity;
+		}
+
+		T* value = head++;
+		T* head_ptr = head.load();
+		if (head_ptr == buffer_end) {
 			head = buffer;
 		}
-		
-		count++;
-#ifdef _DEBUG
-		if (count > capacity) {
-			//whoa there, we're losing events. size up that buffer!
-			//throw 0;
-			lost++;
-		}
-#endif
-
-		return head++;
+		return value;
 	}
 	
-
-	inline T* PopEvent() {
-		if (tail == frame_head) {
-			//Nothing to get.
-			return nullptr;
+	inline void BeginRead() {
+		//Begin critical section
+		read_lock.lock();
+		while (writing) {
+			write_done.wait(read_lock);
 		}
-		if (tail == buffer_end) {
-			tail = buffer;
+		reading = true;
+	}
+	inline bool TryRead() {
+		if (read_lock.try_lock()) {
+			while (writing) {
+				write_done.wait(read_lock);
+			}
+			reading = true;
+			return true;
 		}
-		
-
-		return tail++;
+		return false;
 	}
 
-	inline size_t GetNumberLost() {
-		return lost;
+	inline void EndRead() {
+		reading = false;
+		writing = true;
+		read_lock.unlock();
+		read_done.notify_one();
+		//End critical section
+	}
+
+	inline T* PopEvent() {
+		if (count == 0) {
+			return nullptr;
+		}
+		count--;
+
+		T* value = tail++;
+		T* tail_ptr = tail.load();
+		if (tail_ptr == buffer_end) {
+			tail = buffer;
+		}
+		return value;
+	}
+
+	inline size_t GetNumberDropped() {
+		return dropped;
 	}
 };
--- Experiments/EventStream/Test.cpp
+++ Experiments/EventStream/Test.cpp
@@ -1,35 +1,53 @@
 #include "EventStream.h"
 #include <iostream>
+#include <thread>
 
 __declspec(align(16)) struct TestEvent {
-	__m128 vec;
+	int x;
+	int frame;
 };
 
+void write(EventStream<TestEvent>* stream) {
+	for (int n = 0; n < 10; n++) {
+		stream->BeginWrite();
+		std::cout << "Began writing frame " << n << std::endl;
+		for (int i = 0; i < 10; i++) {
+			TestEvent* ev_in = stream->PushEvent();
+			ev_in->x = i;
+			ev_in->frame = n;
+			std::cout << "wrote " << ev_in->x << " during frame " << ev_in->frame << std::endl;
+		}
+		std::cout << "Done writing frame " << n << std::endl;
+		stream->EndWrite();
+	}
+	
+}
+
+void read(EventStream<TestEvent>* stream) {
+	for (int n = 0; n < 10; n++) {
+		stream->BeginRead();
+		std::cout << "Began reading frame " << n << std::endl;
+		for (int i = 0; i < 10; i++) {
+			TestEvent* ev_out = stream->PopEvent();
+			if (ev_out != nullptr) {
+				std::cout << "read " << ev_out->x << " from frame " << ev_out->frame << std::endl;
+			}
+		}
+		std::cout << "Done reading frame " << n << std::endl;
+		stream->EndRead();
+	}
+}
 
 int main(int argc, char* argv[]) {
-	EventStream<TestEvent> stream(7);
+	EventStream<TestEvent> stream(5);
 
-	stream.BeginWrite();
-	__declspec(align(16)) float zero = 0.0f;
-	__m128 sum = _mm_load_ps(&zero);
-	for (int i = 0; i < 10; i++) {
-		TestEvent* ev_in = stream.PushEvent();
-		//ev_in->x = i;
-		__declspec(align(16)) float vec = (float)i * 10.0f;
-		ev_in->vec = _mm_load1_ps(&vec);
-		sum = _mm_add_ps(sum, ev_in->vec);
-	}
-	stream.EndWrite();
+	std::thread write_thread(write, &stream);
+	std::thread read_thread(read, &stream);
 
-	for (int i = 0; i < 10; i++) {
-		TestEvent* ev_out = stream.PopEvent();
-		if (ev_out != nullptr) {
-		//	std::cout << "out: " << ev_out->x << std::endl;
-			std::cout << "vec: " << ev_out->vec.m128_f32[0] << std::endl;
-		}
-	}
-	std::cout << "Sum: " << sum.m128_f32[0] << std::endl;
-	std::cout << "Lost: " << stream.GetNumberLost() << std::endl;
+	stream.Initialize();
+
+	write_thread.join();
+	read_thread.join();
 
 	//yucky
 	system("PAUSE");
