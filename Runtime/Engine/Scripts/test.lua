c9a690426e70cc022e03ce758d7d752ccf2b0a89
Kyle Emmerich <emmerich.kyle@gmail.com>
2016-01-05 20:14:25 -0600

    Updated Lua test file

--- Runtime/Engine/Scripts/test.lua
+++ Runtime/Engine/Scripts/test.lua
@@ -1,17 +1,1 @@
 print("Hello from LuaJIT 2.0.4")
-
-
-local foo = Vector4.new(10, 10, 10, 1)
-local bar = Vector4.new(0, 5, 20, 1)
-
-print("foo", foo.X, foo.Y, foo.Z, foo.W)
-print("bar", bar.X, bar.Y, bar.Z, bar.W)
-
-local baz = foo * bar
-
-print("baz", baz.X, baz.Y, baz.Z, baz.W)
-
-
-print(foo.Lerp)
-local fup = foo:Lerp(bar, 0.5)
-print("fup", fup.X, fup.Y, fup.Z, fup.W)
