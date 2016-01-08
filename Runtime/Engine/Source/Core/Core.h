0aaf1f3cc59266a1326a618915564249c3f22afc
Kyle Emmerich <emmerich.kyle@gmail.com>
2016-01-08 00:28:07 -0600

    Added porting page, I think?

--- Runtime/Engine/Source/Core/Core.h
+++ Runtime/Engine/Source/Core/Core.h
@@ -18,3 +18,27 @@
  *	
  *	Please note that this documentation is only for the C++ API and does not include Lua API information.
  */
+
+/*! \page porting Porting Blueshift
+	
+	\section intro_sec Porting
+
+	If you're reading this, you're probably cursing me for not porting Blueshift to your favored operating system.
+	However, I'm just one guy and I have a full-time job developing someone else's game engine entirely! Blueshift
+	remains my favorite personal project of all time and while I do love dedicating lots of time to it, the simple
+	fact is that I really don't know much about operating systems other than Windows.
+
+	If you would be interested in helping to port Blueshift to another operating system, it would be much appreciated!
+	In order to port Blueshift, there are quite a few things that need to be added. Fortunately, I've written everything
+	to take drop-in replacements, so the code you need to write is very contained.
+
+	\section components Components to be replaced
+
+	As a rough overview, Blueshift requires platform-specific code for the Core::Timing::IStopwatch interface, 
+	for Graphics::DisplayInfo, for Graphics::IWindow, and most of the Input namespace.
+
+	I will update this page as time goes on with more information, including how to port these components. For now,
+	feel free to jump in and figure out how my existing Windows code works, and let me know if you have any questions
+	using Github's Issues for the project's repository.
+
+*/
